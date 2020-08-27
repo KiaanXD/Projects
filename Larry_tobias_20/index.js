5103547eaceb112655d47f948fb7ae93bf4fe70b
KiaanXD <koonkappa@gmail.com>
2020-08-27 15:50:18 -0400

    added sticky nav and navbr animat w color changes

--- Larry_tobias_20/index.js
+++ Larry_tobias_20/index.js
@@ -1,7 +1,14 @@
+const navitem = document.querySelectorAll('.navitem');
 const toggleBtn = document.getElementsByClassName('toggle-btn')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 let menuOpen = false;
 navbarLinks.classList.remove('active');
+
+// navitem.forEach(item => item.addEventListener('click', navCurrent))
+
+// function navCurrent() {
+//     this.classList.toggle('current');
+// }
 
 toggleBtn.addEventListener('click', () => {
     if (!menuOpen) {
@@ -13,4 +20,9 @@ toggleBtn.addEventListener('click', () => {
         navbarLinks.classList.remove('active');
         menuOpen = false;
     }
+});
+
+window.addEventListener('scroll', () => {
+    let nav = document.querySelector('.navbar');
+    nav.classList.toggle('sticky', window.scrollY > 200);
 });
